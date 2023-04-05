c5ca50f6ba3e57d055ab42e87f3bda7d050289e3
bertrandrw <bertrandrwogera123@gmail.com>
2023-04-05 06:32:21 -0700

    login

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@
 import './App.css';
 import Home from './components/Home';
 import { Routes, Route } from "react-router-dom"
+import Login from './components/Login';
+import Registration from './components/Signup';
+import Signup from './components/Signup.jsx';
+
 function App() {
   return (
 <div className='App'>
 <Routes>
         <Route path="/" element={ <Home/> } />
+        <Route path="login" element={ <Login/> } />
+        <Route path="signup" element={ <Signup/> } />
 
-      </Routes>
+</Routes>
 </div>
   );
 }
